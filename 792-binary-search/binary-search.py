f7405e49a01207a5dee69478eded5c7919664104
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-05-20 19:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (31.56%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -7,8 +7,17 @@ class Solution(object):
         """
         self.nums=nums
         self.target =target
-        for i in range (0,len(nums)):
-            if nums[i]==target:
-                return i
+        left,right = 0 , len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
         return -1
+        
         
