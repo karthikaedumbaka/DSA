1227db5401f00b50108799f82888023f8698780d
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-05-27 22:50:45 +0530

    Time: 11 ms (25.98%) | Memory: 12.5 MB (13.27%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,19 @@
+
+class Solution(object):
+    def maxFreqSum(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        vol=["a","e","i","o","u"]
+        u={}
+        c={}
+        for i in s:
+            if i in vol:
+                u[i]=+u.get(i,0)+1
+            else:
+                c[i]=+c.get(i,0)+1
+        max_vowel = max(u.values()) if u else 0
+        max_consonant = max(c.values()) if c else 0
+
+        return max_vowel+max_consonant
