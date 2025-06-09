f812853ed6b22c8db1ce166ea9040205dce51f72
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-06-09 18:20:57 +0530

    Time: 11 ms (45.43%) | Memory: 12.4 MB (83.88%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,12 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # storing the values for the roman keys in dict
         d={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+
         n=len(s)
         summ=0
         i=0
         while  i<n:
+            # check the i is lessthan n-1 and also check the next roman ltters is lessthan it 
             if i<n-1 and d[s[i]]<d[s[i+1]]:
+                
                 summ+= d[s[i+1]] -d[s[i]]
                 i+=2
             else:
