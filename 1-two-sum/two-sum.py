782514b6992026d54145617e83294aa12eed317b
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-06-14 10:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (89.98%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,9 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # BF
-        for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
+        '''# BF
+        for i in range(0,len(nums)): ##### O(n) itis check every values in list
+            for j in range(i+1,len(nums)): ##### O(n) itis check every values in list after i 
+                if nums[i]+nums[j]==target: # o(1)
                     return [i,j]
-        
+         # time O(n2)
+         # space O(1)'''
+        #  target  = nums[i]+nums[j]
+        #  nums[i]=target-nums[j]
+        #  remaining  = target - values
+
+        d= dict()
+        for index,value in enumerate(nums):
+            remaining = target - value
+            if remaining in d:
+                return([ d[remaining],index ])
+            else:
+                d[value] = index
+        return [-1,-1]
