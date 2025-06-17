8f7fe7f19d66dfe627290bade61f5321cf7d7124
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-06-17 23:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (92.01%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,3 +1,5 @@
+starting_point_for_right=0 
+
 def binary_sreach(nums,target, l , r, first_occ):
     l=0
     r=len(nums)-1
@@ -5,6 +7,9 @@ def binary_sreach(nums,target, l , r, first_occ):
     while l<=r:
         m = (l+r)//2
         if nums[m] == target:
+            if ans==-1:
+                starting_point_for_right=m
+
             ans = m
             if first_occ:
                 r=m-1
@@ -19,7 +24,11 @@ def binary_sreach(nums,target, l , r, first_occ):
 
 class Solution(object):
     def searchRange(self, nums, target):
-        return [binary_sreach(nums,target,0,len(nums)-1,True),binary_sreach(nums,target,0,len(nums)-1,False)]
+        ans_lef=binary_sreach(nums,target,0,len(nums)-1,True)
+        if ans_lef==-1:
+            return [-1,-1]
+
+        return [ans_lef,binary_sreach(nums,target,starting_point_for_right,len(nums)-1,False)]
 
 
 # Time O(log n)
