927802bf7bf8e120cad209dd0fb5ef9327cee39c
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-06-29 08:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.45%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -5,16 +5,14 @@ class Solution:
         """
         x,y = m-1,n-1
         for z in range(m+n -1 ,-1,-1 ):
-            if x<0:
-                nums1[z] = nums2[y]
-                y-=1
-            elif y<0:
+            
+            if y<0:
                 break 
-            elif nums1[x] < nums2[y]:
-                nums1[z] = nums2[y]
-                y-=1
-            else:
+            elif x>=0 and nums1[x] > nums2[y]:
                 nums1[z] = nums1[x]
                 x-=1
+            else:
+                nums1[z] = nums2[y]
+                y-=1
 
         
