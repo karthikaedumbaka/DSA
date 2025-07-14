fb6103bd1e9afec290dcba0bb89845e3665e1234
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-07-14 20:01:10 +0530

    Time: 21 ms (39.89%) | Memory: 26.1 MB (17.95%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,12 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        d={}
-
-        for num in nums:
-            # d.get fucntion will appends the element if is not there in dictionary 
-            d[num]=d.get(num,0)+1
-            if d[num]>1:
-                return True
+        '''# BF 
+        # i will use 2 for loop , first loop for pick the numbder and 
+        #second loop for to chek it is repeating or not 
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    return True
         return False 
-        
+        # time 0(n^2)
+        # space  0(1)'''
+        '''
+        # but on optimal solution we can use the dictionary 
+        d={}
+        for i in nums:
+            d[i] = d.get(i,0)+1
+            if d[i] > 1:
+                return True
+        return False '''
+
+        s =set()
+        for i in nums:
+            if i in s:
+                return True
+            else:
+                s.add(i)
+        return False        
