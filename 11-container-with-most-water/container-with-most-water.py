a0a0f436e952d2a4c52d2435e05afedd218b7b02
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-07-20 13:51:41 +0530

    Time: 107 ms (38.43%) | Memory: 28.5 MB (67.34%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,6 +4,10 @@ class Solution:
         i,j =0,len(height)-1
         ans = 0 
         while i < j :
+            if  height[i] ==0:
+                i+=1
+            elif height[j] ==0:
+                j-=1
             width =j-i
             a=min(height[i],height[j])
             ans=max(ans,width*a)
