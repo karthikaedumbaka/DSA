beba8681973bdfe1ec93f4a3b419f259564b7c52
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-07-26 23:04:14 +0530

    Time: 26 ms (50.26%) | Memory: 19.4 MB (74.85%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n=len(nums) # to get the lenght of arr
+        stk=[] # for stacks 
+        ans=[-1]* n 
+
+        for i in range (2*n -1,-1, -1):
+            while stk and stk[-1] <= nums[i%n] :
+                stk.pop()
+            if i<n:
+                if stk:
+                    ans[i] = stk[-1]
+            stk.append(nums[i%n])
+        return ans
+        
+        
