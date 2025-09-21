4c9d179846016a073458b4bb2c1ab351cc663ce2
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-09-21 22:50:18 +0530

    Time: 2 ms (17.62%) | Memory: 12.4 MB (50.79%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         """
@@ -5,15 +6,26 @@ class Solution(object):
         :type stones: str
         :rtype: int
         """
-        stones_dict =dict()
-        output = 0 
-        for stone in stones:
-            if stone in stones_dict:
-                stones_dict[stone]+=1
-            else:
-                stones_dict[stone]=1
+        # stones_dict =dict()
+        # output = 0 
+        # for stone in stones:
+        #     if stone in stones_dict:
+        #         stones_dict[stone]+=1
+        #     else:
+        #         stones_dict[stone]=1
         
-        for jewel in jewels:
-            if jewel in stones_dict:
-                output+=stones_dict[jewel]
-        return output
+        # for jewel in jewels:
+        #     if jewel in stones_dict:
+        #         output+=stones_dict[jewel]
+        # return output
+        stones_dict = Counter(stones)
+        jewels_set=set(jewels)
+        ans=0
+        for stone,freq in stones_dict.items():
+            if stone in jewels_set:
+                ans+=freq
+        return ans
+
+# Time Complexity: O(n + m)
+
+# Space Complexity: O(m + u) ≈ O(m)
