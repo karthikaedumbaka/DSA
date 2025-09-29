8f6ba84838aa009979e4de95348224ec62844d36
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-09-29 09:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.30%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -6,4 +6,14 @@ class Solution:
                 nums[k]= nums[i]
                 k+=1
         return k
+
+'''
+val = 3
+k=0 , i =0 , num[i] != val -> 3 != 3 (False) => [3,2,2,3]
+k=0 , i = 1 ,num[i] != val -> 2 != 3 (True)  ==> [2,2,2,3] ......k=1
+k=1 ,i=2 ,num[i] != val -> 2 != 3 (True)  ==> [2,2,2,3] ......k=2
+k=1 ,i=3 ,num[i] != val -> 3 != 3 (False)  ==> [2,2,2,3] ......k=2
+
+
+'''
         
