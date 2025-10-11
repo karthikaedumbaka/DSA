8b2cd3b10e0ef03de9a1f7552a493343489ffcf7
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-10-12 00:50:21 +0530

    Time: 12 ms (17.12%) | Memory: 22.3 MB (81.50%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,9 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k = k % n  # Handle cases where k > n
-        nums[:] = nums[-k:] + nums[:-k]
-        return nums
-        # time o(n)
+        # n = len(nums)
+        # k = k % n  # Handle cases where k > n
+        # nums[:] = nums[-k:] + nums[:-k]
+        # return nums
+        # # time o(n)
         #space O(1)
+
+
+        n=len(nums)
+        k= k%n
+
+        def reveal(l,r):
+            while l<=r:
+                nums[l] ,nums [ r] = nums[r] ,nums[l]
+                l=l+1
+                r=r-1
+        reveal(0,n-1)
+        reveal(0,k-1)
+        reveal(k,n-1)
