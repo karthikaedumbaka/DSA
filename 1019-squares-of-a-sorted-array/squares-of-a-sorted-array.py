37a0e46aedc49ebd425baad84f1cc1f0037f97c1
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-12-25 23:02:18 +0530

    Time: 11 ms (52.88%) | Memory: 19.4 MB (57.12%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -10,27 +10,45 @@ class Solution:
         
 
         # best 
-        for i in range(len(nums)):
-            nums[i] = nums[i] ** 2
-        #[16,1,0,9,100]
-        # print(nums)
+        # for i in range(len(nums)):
+        #     nums[i] = nums[i] ** 2
+        # #[16,1,0,9,100]
+        # # print(nums)
+        # l = 0
+        # r = len(nums)-1
+        # ans = []
+        # while  l <= r :
+        #     if nums[l] < nums[r]:
+        #         ans.append(nums[r])
+        #         r-=1
+        #     else : 
+        #         ans.append(nums[l])
+        #         l+=1
+        # # swaping
+        # a=0
+        # b=len(ans)-1
+        # while a<b:
+        #     ans[a] ,ans[b] = ans[b] ,ans[a]
+        #     a+=1
+        #     b-=1
+        # return ans
+
+        # the best  
+        # [-4,-1,0,3,10]
+        ans = [0] * len(nums)
         l = 0
         r = len(nums)-1
-        ans = []
-        while  l <= r :
-            if nums[l] < nums[r]:
-                ans.append(nums[r])
+        write = len(nums) -1
+        while l <= r :
+            left = nums[l] ** 2 
+            right =nums[r] ** 2 
+            if left  < right :
+                ans[write] = right
                 r-=1
-            else : 
-                ans.append(nums[l])
+            else:
+                ans[write] = left
                 l+=1
-        # swaping
-        a=0
-        b=len(ans)-1
-        while a<b:
-            ans[a] ,ans[b] = ans[b] ,ans[a]
-            a+=1
-            b-=1
+            write -=1
         return ans
 
 
