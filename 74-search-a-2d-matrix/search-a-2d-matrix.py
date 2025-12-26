696f3cf508a56929d1584276ebef171219a05985
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-12-26 22:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.98%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,32 +1,27 @@
-class Solution(object):
+class Solution:
     def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        row=-1
-        i,j=0,len(matrix)-1
-        while i<=j:
-            mid=(i+j)//2
-            if matrix[mid][0] > target:
-                row=mid-1
-                j=mid-1
-            else:
-                i=mid+1
-
-        l,r=0,len(matrix[row])-1
-        while l<=r:
-            mid=(l+r)//2
-            if matrix[row][mid]==target:
+        row = -1
+        t = 0
+        b = len(matrix) -1
+        while t <=b :
+            mid = (t+b) //2 
+            if matrix[mid][0] <=  target  <= matrix[mid][-1]:
+                row = mid
+                break
+            elif  matrix[mid][0] >  target:
+                b = mid -1
+            else :
+                t =mid +1
+        if row == -1:
+            return False
+        l = 0 
+        r = len(matrix[row])-1
+        while l <=r :
+            mid = (l+r)//2
+            if matrix[row][mid] == target:
                 return True
-            elif matrix[row][mid]>target:
-                r=mid-1
+            elif matrix[row][mid] >target:
+                r = mid -1
             else:
-                l=mid+1
-        return False 
-            
-
-
-
-        
+                l = mid +1
+        return False
