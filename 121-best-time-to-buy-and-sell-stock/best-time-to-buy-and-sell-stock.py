427bdee8430d6d90ec28a7de3ff5b203b7d1e6fe
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2025-12-27 19:50:48 +0530

    Time: 143 ms (12.12%) | Memory: 26.4 MB (94.63%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,8 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", 'w').write('0'))
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # by using Sliding window
-        l,r=0,1
-        profit=0
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = max(profit,prices[r]-prices[l])
-            else:
-                l=r
-            r+=1
-        return profit
-
-        # time O(n)
-        # space O(1)
+        max_profit =0
+        min_price = prices[0]
+        for i in prices[1:]:
+            max_profit = max(max_profit,i - min_price)
+            min_price = min(i,min_price)
+        return max_profit
