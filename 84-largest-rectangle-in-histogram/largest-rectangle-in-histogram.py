0b63ac55bdb1a2b62ad613b84ff2e4b88a0a04d2
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-15 22:22:59 +0530

    Time: 179 ms (26.06%) | Memory: 31.7 MB (38.19%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,59 +1,60 @@
 class Solution:
-    def next_smaller(self,arr: List[int]) -> List[int]:
-        stk = []
-        n=len(arr)
-        ans = [n for i in arr]
-        for i in range(n-1,-1,-1):
-            while len(stk) >0 and arr[stk[-1]] >= arr[i]:
-                stk.pop()
-            if len(stk) >0:
-                ans[i] = stk[-1]
-            stk.append(i)
-        return ans
-    def prev_smaller(self,arr: List[int]) -> List[int]:
-        stk = []
-        n=len(arr)
-        ans = [-1 for i in range(n)]
-        for i in range(n):
-            while len(stk) >0 and arr[stk[-1]] >= arr[i]:
-                stk.pop()
-            if len(stk) >0:
-                ans[i] = stk[-1]
-            stk.append(i)
-        return ans
-
-
-
     def largestRectangleArea(self, heights: List[int]) -> int:
-        '''# your rectangle can cover area from mulitple bar
-        # For BruteForce , we can check with all the possible rectangle -- ? 
-        # rectangle  has 2 thigs , height , width
-        # traverse over the array , for each i asume that to be the height of rectangle 
-        ans=0
-        for i in range(len(heights)):
-            height= heights[i]
-            left = i-1
-            right = i+1
-            while left >=0 and heights[left] >= height:
-                left -=1
-            while right <len(heights) and heights[right] >= height:
-                right +=1
-            width = right - left - 1
-            ans=max(ans,height * width)
+        # BF
+        # max_arae = 0
+        # l = len(heights)
+        # for i in range(l):
+        #     height = heights[i]
+        #     left ,right = i-1 ,i+1
+        #     while left >=0 and heights[left] >= heights[i]:
+        #         left -=1
+        #     while right < l and heights[right] >= heights[i]:
+        #         right +=1
+        #     width = right - left -1
+        #     max_arae =max(max_arae ,height * width )
+        # return max_arae
+        '''
+        if you see the pattern we can see 
+        left is prev_smaller
+        right is next_smaller
+
+        '''
+        ans = 0 
+        l= len(heights)
+        left = self.left_height(heights)
+        right = self.right_height(heights)
+        for i in range(l):
+            width = right[i] - left[i]  -1
+            ans =max(ans ,width *heights[i] )
         return ans
-        # time O(n^2)
-        # space O(1)'''
-        ans =0
-        next_smaller = self.next_smaller(heights)
-        prev_smaller = self.prev_smaller(heights)
-        for i in range(len(heights)):
-            height = heights[i]
-            width = next_smaller[i] -prev_smaller[i] -1
-            ans = max(ans,height*width) 
-        return ans
-        
 
-
-
+    def left_height(self,heights):
+        l = len(heights)
+        result = [-1]* l
+        # why -1 see if we donot have the any small 
+        stk = [ ]
+        for i in range(l) :
+            while stk  and heights[stk[-1]] >= heights[i]:
+                stk.pop()
+            if stk:
+                result[i] = stk[-1]
+            stk.append(i)
+        return result
     
+    def right_height(self,heights):
+        l = len(heights)
+        result = [l]* l
+        stk = [ ]
+        for i in range(l):
+            while stk and heights[stk[-1]] > heights[i]:
+                idx = stk.pop()
+                result[idx] = i
+            stk.append(i)
+        return result
+    
+
         
+            
+
+        
+
