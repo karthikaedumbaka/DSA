69981a1ef1d2c69bd740d9f50eb2aacc69afeec3
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-16 23:18:36 +0530

    Time: 75 ms (69.10%) | Memory: 22.2 MB (8.82%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,44 +1,21 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        # n=len(nums)
-        # ans=0
-        # for i in range(n):
-        #     for j in range(i,n):
-        #         temp=k
-        #         is_valid=True
-        #         for z in range(i,j+1):
-        #             if nums[z] ==0:
-        #                 if temp>0:
-        #                     temp-=1
-        #                 else:
-        #                     is_valid=False
-        #                     break
-        #         if is_valid:
-        #             ans=max(ans,j-i+1)
-        # return ans
-        max_length = 0  # Maximum subarray length
-        left = 0        # Left boundary of sliding window
-        right = 0       # Right boundary of sliding window
-        zeros = 0       # Count of zeros in current window
-        n = len(nums)
-        
-        while right < n:
-            # Expand window to the right
+        left = 0 
+        right = 0 
+        zeros = 0
+        ans= 0
+        while right < len(nums):
             if nums[right] == 0:
-                zeros += 1
-                
-            # Contract window from the left if too many zeros
-            while zeros > k:   # if also whe can use
-                if nums[left] == 0:
-                    zeros -= 1
-                left += 1
-                
-            # Update maximum length
+                zeros +=1
+            while zeros > k :
+                if nums[left] ==0:
+                    zeros -=1
+                left +=1
             if zeros <= k:
-                length = right - left + 1
-                max_length = max(max_length, length)
-                
-            # Continue expanding the window
-            right += 1
+                ans =max(ans,right - left +1)
+            right +=1
+
+        return ans
+
             
-        return max_length
+
