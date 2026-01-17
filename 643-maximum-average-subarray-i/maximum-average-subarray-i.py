ca59feac12e18d9a2b8101f3d37208127641b140
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-17 16:32:00 +0530

    Time: 79 ms (60.22%) | Memory: 29.5 MB (6.78%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,21 +1,36 @@
-
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
-    def findMaxAverage(self, nums, k):
-        curr_avg =0
+    def findMaxAverage(self, nums: List[int], k: int) -> float:\
+    # BF
+    #time O(N*K)
+    # space O(1)
+        # max_avg = float("-inf")
+        # n=len(nums)
+        # if len(nums) == 1 :
+        #     return nums[0]
+
+        # for i in range(n-k+1):
+        #     k_size = 0
+        #     for j in range(i,i+k):
+        #         k_size += nums[j]
+        #     k_avg = k_size/k
+        #     max_avg = max(max_avg,k_avg)
+        
+        # return max_avg
+
+        ans = float("-inf")
         n=len(nums)
+        first_k=0
         for i in range(k):
-            curr_avg+=nums[i]
-        max_avg = curr_avg /k
+            first_k += nums[i]
+        k_avg = first_k / k 
+        ans = max(ans,k_avg )
 
         for i in range(k,n):
-            curr_avg +=nums[i]
-            curr_avg -= nums[i-k]
-            avg=curr_avg/k
-            max_avg=max(avg,max_avg)
+            first_k += nums[i]
+            first_k -= nums[i-k]
+            k_avg = first_k / k
+            ans = max(ans,k_avg )
+        return ans
 
-        return max_avg
-        #time 0(n)
-        #space 0(1)
 
-        
+    
