ad3cf423a73b8a3089aaad50b61e2f9694aca397
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-20 22:26:23 +0530

    Time: 9 ms (39.27%) | Memory: 23.2 MB (6.83%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,24 +1,20 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort() # to sort the intervals 
-        ans=[]
-        i=0
-        while i < len(intervals):
-            s=intervals[i][0]  
-            e=intervals[i][0]
-            while  i < len(intervals) and intervals[i][0]<=e:
-                # Dry run
-                '''
-                i=0 -> s=1,e=3 -> max: 3 == e;
-                i=1 -> intervals[1][0]=2 ,e=3 -> max=6;
-                i=2 ->  intervals[2][0]=8 , e=6 -> not overlap -> so append
-                i=3 ->  intervals[3][0]=15 , e=6 -> not overlap -> so append
-
-                '''
-                e=max(e,intervals[i][1])
+        #first we will sort the value , because while we are campering the starting and ending pointer of the list we need the sort value right 
+        intervals.sort() # sorted the value # Time  O(NlogN)
+        ans = []
+        i =0
+        while i < (len(intervals)):
+            #s is for startng point of the interval and e is the ending pointer of interval 
+            # new we are check is out curr end is  greater than the cuur_next starting pointer if yes just meage by using max for the curr ending point to curr_next ending point 
+            s = intervals[i][0]
+            e = intervals[i][1]
+            while i < len(intervals) and intervals[i][0] <= e:
+                e = max (e,intervals[i][1])
                 i+=1
+            # if there is no Merging point of cuur and curr_next , append the s and e pointer
             ans.append([s,e])
         return ans
-# TC : O(NlogN)
-#space O(N)
+
+
         
