e9ee15f4ef48c4fcaf8b6da85e1f2f25d722b753
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-22 10:36:06 +0530

    Time: 287 ms (33.40%) | Memory: 19.2 MB (17.44%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,7 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <=1:
+        if n <=1 :
             return n
         return self.fib(n-1) + self.fib(n-2)
-        # time 2^n
-        #space 2^n
