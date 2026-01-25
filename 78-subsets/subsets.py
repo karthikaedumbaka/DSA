7b5191397bc9f286c2911ba91d2d7b1e472af52b
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-25 20:35:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (10.47%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def solve(self,index:int,nums:List[int],sub_set:List[int],result:List[List[int]]) :
+        if index >= len(nums):
+            result.append(sub_set.copy())
+            return
+        sub_set.append(nums[index])
+        self.solve(index + 1,nums,sub_set,result)
+        sub_set.pop()
+        self.solve(index + 1,nums,sub_set,result)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        self.solve(0,nums,[],result)
+        return result
