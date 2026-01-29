8fb9b20fdda61a702ba7088d17b71bf2dd6cebf4
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-29 23:01:04 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (17.78%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,16 @@
-
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for  index ,val in enumerate(nums) :
-            req = target - val 
-            if req in d :
-                return [index , d[req]]
-            d[val] = index
+        #to store the visit values with idex as value 
+        d=defaultdict(int)
+        # we are using enumarate funtion to get the both index and value form the input 
+        for  index,value in enumerate(nums):
+            # we are check if our reqq is present in dicti or not 
+            req = target - value
+            # if it present and check the req index and currect index are not equal  then 
+            if req in d  and d[req] != index:
+                # we are return the output
+                return [d[req] , index]
+            # and the see vale with index to dictionary
+            d[value] = index
         return [-1,-1]
-
-        
