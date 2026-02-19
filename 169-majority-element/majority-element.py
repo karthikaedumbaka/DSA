97fd19af91c6695b482ce8602ee09fff8c30377b
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-02-19 08:47:48 +0530

    Time: 13 ms (22.61%) | Memory: 21.2 MB (37.11%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,13 @@
+from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Boyer-Moore Voting Algorithm  (it have half the element is same)
-        result = count =0
+        majority_value = (len(nums)//2)+1
+        count_num = defaultdict(int)
+
         for num in nums:
-            if count == 0 :
-                result = num
-            count = count + ( 1 if num == result else -1)
-        return result
-        # time 0(n)
-        # space 0(1)
+            count_num[num] +=1
+            if count_num[num] >= majority_value:
+                return num
+        return [-1,-1]
 
         
