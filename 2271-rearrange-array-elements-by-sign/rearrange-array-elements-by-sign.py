1712e48dc3b4d5277fabf4e902a536acf7b21a39
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-02-19 14:36:35 +0530

    Time: 43 ms (73.68%) | Memory: 42.9 MB (65.81%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,15 +1,35 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        positive_num = []
-        negative_num = []
-        aforementioned_conditions = []
-        for num in nums:
-            if num < 0 :
-               negative_num.append(num)
-            else:
-                positive_num.append(num)
+        # positive_num = []
+        # negative_num = []
+        # aforementioned_conditions = []
+        # for num in nums:
+        #     if num < 0 :
+        #        negative_num.append(num)
+        #     else:
+        #         positive_num.append(num)
         
-        for idx in range(len(positive_num)):
-            aforementioned_conditions.append(positive_num[idx])
-            aforementioned_conditions.append(negative_num[idx])
-        return aforementioned_conditions
+        # for idx in range(len(positive_num)):
+        #     aforementioned_conditions.append(positive_num[idx])
+        #     aforementioned_conditions.append(negative_num[idx])
+        # return aforementioned_conditions
+        # time O(n) * O(n) -> O(2n) -> O(n)
+        # space O(n/2) +O(n/2) -> O(n)
+        n = len(nums)
+        ans = [0] *n
+        positive_idx = 0
+        negative_idx = 1
+        for num in nums:
+            if num < 0:
+                ans[negative_idx] = num
+                negative_idx +=2
+            else:
+                ans[positive_idx] = num
+                positive_idx +=2
+        return ans
+                
+            
+
+
+
+
