7aa60b479a34ff4c3caf7bfbab5229336ef9fc9e
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-02-20 09:03:13 +0530

    Time: 4 ms (37.49%) | Memory: 20.1 MB (50.46%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -25,22 +25,58 @@ class Solution:
         #             matrix[r][c] = 0
         
         # return matrix
+        #time O(n**3)
+        # space O(1)
+
         # better slo
-        col = len(matrix[0])
-        row = len(matrix)
-        col_m = [0]* col
-        row_m = [0]* row
-        for r in range(row):
-            for c in range(col):
-                if matrix[r][c] == 0:
-                    row_m[r] = 1
-                    col_m[c] = 1
-        for r in range(row):
-            for c in range(col):
-                if (row_m[r] ==1 or col_m[c] ==1 ):
-                    matrix[r][c] = 0
+        # col = len(matrix[0])
+        # row = len(matrix)
+        # col_m = [0]* col
+        # row_m = [0]* row
+        # for r in range(row):
+        #     for c in range(col):
+        #         if matrix[r][c] == 0:
+        #             row_m[r] = 1
+        #             col_m[c] = 1
+        # for r in range(row):
+        #     for c in range(col):
+        #         if (row_m[r] ==1 or col_m[c] ==1 ):
+        #             matrix[r][c] = 0
         
-        return matrix
+        # return matrix
+        # time  (n*m)
+        # space (n*m)
+
+        # best solution
+
+        row = len(matrix)
+        col = len(matrix[0])
+        ## col[m] -> matrix[0][...]
+        ## rol[n] -> matrix[...][0]
+        col_index = 1
+        for n in range(row):
+            for  m in range(col):
+                if matrix[n][m] ==0 :
+                    matrix[n][0] = 0
+                    if (m != 0):
+                        matrix[0][m] = 0
+                    else:
+                        col_index = 0
+        
+        for n in range(1,row):
+            for m in range(1,col):
+                if matrix[n][m] !=0 :
+                    if matrix[0][m] ==0 or matrix[n][0] ==0:
+                        matrix[n][m] =0
+        
+        if matrix[0][0] == 0:
+            for m in range(col):
+                matrix[0][m] =0 
+        if col_index == 0:
+            for n in range(row):
+                matrix[n][0] =0
+
+
 
 
                            
