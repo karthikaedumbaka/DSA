ab27c56fb3c39396113d6ff10b691fdcb991e5ee
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-02-23 10:53:05 +0530

    Time: 7 ms (54.65%) | Memory: 27.2 MB (6.69%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -16,8 +16,27 @@ class Solution:
         BF we can do pop form right and insert on left in a list insert takes O(n) time for every element 
         so ti will take O(n*k)
         '''
-        for _ in range(k):
-            pop_element = nums.pop()
-            nums.insert(0, pop_element)
+        # for _ in range(k):
+        #     pop_element = nums.pop()
+        #     nums.insert(0, pop_element)
+        '''
+        time O(n*k)
+        space O(1)
+        '''
+        n = len(nums)
+        k = k%n 
+        if n < k :return
+        def rotateHelper(l,r):
+            while l<=r:
+                nums[l] ,nums[r] =nums[r] ,nums[l]
+                l+=1
+                r-=1
+        rotateHelper(0,n-1)
+        rotateHelper(0,k-1)
+        rotateHelper(k,n-1)
+            
+        
+
+
         
 
