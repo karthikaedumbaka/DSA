3fe1d3eb066b26d86e6619d51576939a79b1635c
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-02-23 10:47:35 +0530

    Time: 1742 ms (5.01%) | Memory: 26.4 MB (73.62%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,17 +1,23 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k = k % n
+        # n=len(nums)
+        # k= k%n
+        # if k ==0 : return 
 
-        def rev(l, r):
-            while l < r:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
+        # def reveal(l,r):
+        #     while l<=r:
+        #         nums[l] ,nums [ r] = nums[r] ,nums[l]
+        #         l=l+1
+        #         r=r-1
+        # reveal(0,n-1)
+        # reveal(0,k-1)
+        # reveal(k,n-1)
+        ''' for this we need to rotate the element for k step 
+        BF we can do pop form right and insert on left in a list insert takes O(n) time for every element 
+        so ti will take O(n*k)
+        '''
+        for _ in range(k):
+            pop_element = nums.pop()
+            nums.insert(0, pop_element)
+        
 
-        # 1) Reverse entire array
-        rev(0, n - 1)
-        # 2) Reverse first k elements
-        rev(0, k - 1)
-        # 3) Reverse remaining n - k elements
-        rev(k, n - 1)
