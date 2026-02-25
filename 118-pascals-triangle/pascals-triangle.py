b117005cbec6fda17082a18e44c359c7bd4e6b48
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-02-25 21:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.16%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,13 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = [[1]] 
-        for i in range(numRows-1):
+        res = [ [1] ]
+        for i in range(numRows -1):
             temp = [0] + res[-1] + [0]
             row = []
-            for j in range(len(res[-1])+1):
+            for j in range(len(res[-1]) +1) :
                 row.append(temp[j] + temp[j+1])
             res.append(row)
         return res
-
-#time O(n^2)
-#time O(n)
