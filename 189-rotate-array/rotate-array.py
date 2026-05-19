4967292f34baf317da30d5a072f89d1bdc51baf8
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-05-19 10:53:48 +0530

    Time: 6 ms (61.49%) | Memory: 26.8 MB (19.33%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,42 +1,19 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        # n=len(nums)
-        # k= k%n
-        # if k ==0 : return 
-
-        # def reveal(l,r):
-        #     while l<=r:
-        #         nums[l] ,nums [ r] = nums[r] ,nums[l]
-        #         l=l+1
-        #         r=r-1
-        # reveal(0,n-1)
-        # reveal(0,k-1)
-        # reveal(k,n-1)
-        ''' for this we need to rotate the element for k step 
-        BF we can do pop form right and insert on left in a list insert takes O(n) time for every element 
-        so ti will take O(n*k)
-        '''
-        # for _ in range(k):
-        #     pop_element = nums.pop()
-        #     nums.insert(0, pop_element)
-        '''
-        time O(n*k)
-        space O(1)
-        '''
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         n = len(nums)
         k = k%n 
         if n < k :return
-        def rotateHelper(l,r):
-            while l<=r:
-                nums[l] ,nums[r] =nums[r] ,nums[l]
-                l+=1
+        def helper(l,r):
+            while  l<=r:
+                nums[l],nums[r] = nums[r] ,nums[l]
+                l+=1 
                 r-=1
-        rotateHelper(0,n-1)
-        rotateHelper(0,k-1)
-        rotateHelper(k,n-1)
-            
-        
-
+        helper(0,n-1)
+        helper(0,k-1)
+        helper(k,n-1)
+        return nums
 
         
-
