24bef2d8a6970a0bdd1de2c3b387d59785c918b8
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-05-19 10:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (52.76%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -2,11 +2,17 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         n = len(nums)
         count = 1
-        for i in range(1,2*n):
-            if nums[i%n] >= nums[(i-1)%n]:
-                count+=1
+        if n==1 or n==0:
+            return True
+            
+        for i in range(n*2):
+            if nums[(i%n)] >= nums[(i-1)%n]:
+                count +=1
             else:
                 count = 1
             if count == n:
                 return True
-        return n==1
+        return False
+
+        # Time : O(n)
+        #space O(1)
