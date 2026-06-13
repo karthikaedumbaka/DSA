7e4beeb55df0412675f99e09197da881a73812c3
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-06-13 07:35:27 +0530

    Time: 54 ms (32.49%) | Memory: 87.1 MB (5.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,19 @@
-class Solution:
+class Solution(object):
     def isPalindrome(self, s):
-        l = 0
+        l = 0 
         r = len(s)-1
-        return self.check(l,r,s)
-    def check(self,l,r,s):
-        if l >= r:
-            return True
-        if not s[l].isalnum():
-            return self.check(l+1,r,s)
-        if not s[r].isalnum():
-            return self.check(l,r-1,s)
-        if (s[l].lower()) != (s[r].lower()):
-            return False
-        return self.check(l+1,r-1,s)
+        s = s.lower()
+        
+
+        def check(s,l,r):
+            if l >= r:
+                return True
+            if not s[l].isalnum():
+                return check(s,l+1,r)
+            if not s[r].isalnum():
+                return check(s,l,r-1)
+            if not s[l] == s[r]:
+                return False
+            else:
+                return check(s,l+1,r-1)
+        return check(s,l,r)
