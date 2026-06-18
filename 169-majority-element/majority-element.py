83f76ac9da6797458726d8acac82fc40d406a8f1
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-06-18 22:05:43 +0530

    Time: 7 ms (57.58%) | Memory: 21 MB (98.48%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,30 +1,19 @@
-from collections import defaultdict
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # majority_value = (len(nums)//2)+1
-        # count_num = defaultdict(int)
-
-        # for num in nums:
-        #     count_num[num] +=1
-        #     if count_num[num] >= majority_value:
-        #         return num
-        # return -1
-
-        # Moore's Voting Algorithm
-
-        majority_value = 0
+        majortiyElement = 0 
         count = 0
-
         for num in nums:
-            if count == 0:
-                majority_value = num
+            if count==0:
+                majortiyElement = num
                 count +=1
-            elif num == majority_value:
-                count +=1
+            elif num == majortiyElement:
+                count+=1
             else:
                 count -=1
-        
-        return majority_value
-
+        return majortiyElement
+            
+    # time O(n)
+    # space O(1)
 
         
