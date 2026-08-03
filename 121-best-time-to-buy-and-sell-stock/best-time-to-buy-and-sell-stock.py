6649cdeabc758f4c1e21deb99afbf60fe0b545f4
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-08-03 16:03:26 +0530

    Time: 32 ms (83.05%) | Memory: 28.6 MB (75.03%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,11 +3,8 @@ class Solution:
         max_profit = 0 
         min_price = float('inf')
         for price in prices:
-            if price < min_price:
-                min_price = price
+            if min_price >  price:
+                min_price = price 
             else:
-                max_profit = max(max_profit , price - min_price)
+                max_profit = max(max_profit, price - min_price )
         return max_profit
-
-        # time O(n)
-        # space O(1)
